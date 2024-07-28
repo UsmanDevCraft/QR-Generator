711372bcc2b2fa730b668278bdf65f3803d74ffd
Usman Awan <uawan8154@gmail.com>
2024-07-28 11:58:22 +0100

    Removed res.sendFile() and put whole html page to avoid in built qr image.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { dirname } from "path"
 import { fileURLToPath } from "url"
 import express from "express"
 import qr from "qr-image"
-import fs from "fs"
 
 const app = express();
 const port = 5000;
@@ -21,12 +20,47 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
     username = req.body.username;
     URL = req.body.url;
-    var qr_svg = qr.image(URL, { type: 'png' });
-    qr_svg.pipe(fs.createWriteStream('QR-image.png'));
     if(username && URL){
-        res.sendFile(__dirname + "/index2.html");
-    }
-    if(!username && !URL){
+    var qr_svg = qr.imageSync(URL, { type: 'png' });
+    const qrCodeBase64 = Buffer.from(qr_svg).toString('base64');
+    const qrCodeImageSrc = `data:image/png;base64,${qrCodeBase64}`;
+        res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <style>
+                body{
+                    width: 100vw;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: center;
+                    overflow-x: hidden;
+                }
+                .container{
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                }
+                .heading{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+            </style>
+            <body>
+                <div class="container">
+                    <h1 class="heading">Here is your QR Code.</h1>
+                    <img src="/QR-image.png" alt="qr-code">
+                    <a href="/QR-image.png" download="QR-image.png"><button>Download Image</button></a>
+                </div>
+            </body>
+            </html>
+        `);
+    } else {
         res.sendFile(__dirname + "/index.html");
     }
 });
